5064f7c3690673e3d972404dd99179e4aa2411ae
MariaGlushtsova <glushtsova@tut.by>
2023-07-18 19:56:02 +0300

    Initial Commit

---
+++ IOS10-HW16-MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS10-HW16-MVP
+//
+//  Created by Admin on 18.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
